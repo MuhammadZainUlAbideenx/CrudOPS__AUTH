c874f0eb94e95d82324c4e93f1dee7940170e536
zain <zainulabideen00945@gmail.com>
2024-06-14 15:12:53 +0500

    m

--- Client/src/Api.js
+++ Client/src/Api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const URL ='http://localhost:8000/';
+
+const URL ='http://localhost:8000';
 export default adduser = async()=>{
 
     await axios.post(`${URL}/add`,data)
--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 
+
 const PORT = 8000;
 const app = express();
+
 app.listen(PORT,console.log(`Server is running successfully on PORT ${PORT}`));
