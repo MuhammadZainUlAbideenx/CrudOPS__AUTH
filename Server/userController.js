fe288ea9119f638ce2fc353aa589618828f7d5c1
zain <zainulabideen00945@gmail.com>
2024-07-03 15:37:49 +0500

    plll

--- Server/userController.js
+++ Server/userController.js
@@ -1,19 +1,17 @@
 import User from './userSchema.js';
 export const addUser = async (req,res,next)=>{
-    const {name, email , password}= req.body;
+    const {username, email , password}= req.body;
     const user = req.body;
     const newUser= new User (user);
     try {
-        let existingUser;
-        existingUser = await User.findOne({email:email});
+      const user = new User({
+        username,
+        email,
+        password
+      });
        
-       res.status(201).json(newUser)
-       if (existingUser){
-        return res.status().json({message:'User Already Exist'})
-        
-    } else{
-      await User.save()
-    }
+      await user.save();
+    
   } 
     catch (error) {
       console.log(error);
