1aa6cef6bb781dc178eabf5009a48ff734980800
zain <zainulabideen00945@gmail.com>
2024-07-04 13:03:20 +0500

    sf

--- Server/Routes.js
+++ Server/Routes.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { signUp } from './userController.js';
+import { signUp ,login } from './userController.js';
 
 const router = express.Router();
 
 router.post("/signUp",signUp);
+router.post("/login",login);
 export default router;
--- Server/userController.js
+++ Server/userController.js
@@ -18,9 +18,21 @@ export const signUp = async (req, res, next) => {
     });
 
     await user.save();
-    res.status(201).json({ message: user });
+    return res.status(201).json({ message: user });
   } catch (error) {
     console.log(error);
   }
 };
-export const login = () => {};
+export const login = async (req,res,next) => {
+  const {email,password} =req.body;
+  try {
+    let userExist= await User.findOne({email:email , password : password})
+    if (userExist){
+      return res.status(200).json({message:"Login Successfull"})
+    } 
+    return res.status(401).json({message:"User Not Found"})
+  } catch (error) {
+    console.log(error)
+  }
+  
+};
